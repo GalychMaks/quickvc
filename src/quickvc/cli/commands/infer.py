0327f99ec838a60871d341c018cdcdfa54873f85
MaksymHalych <galych222@gmail.com>
2025-08-03 10:21:39 +0300

    feat(infer): add rtf computation

--- src/quickvc/cli/commands/infer.py
+++ src/quickvc/cli/commands/infer.py
@@ -1,39 +1,57 @@
 import logging
-import os
 import time
 from pathlib import Path
+from typing import Optional
 
 import soundfile as sf
 import torch
 
+from quickvc.utils.timer import Timer
 from quickvc.wrapper import QuickVC
 
 logger = logging.getLogger(__name__)
 
 
 def infer(
-    config: Path = Path("checkpoints/pretrained/config.yaml"),
-    checkpoint: Path = Path("checkpoints/pretrained/G_1200000.pth"),
+    config_path: Path = Path("checkpoints/pretrained/config.yaml"),
+    checkpoint_path: Path = Path("checkpoints/pretrained/G_1200000.pth"),
     source: Path = Path("data/source.wav"),
     target: Path = Path("data/target.wav"),
-    outdir: Path = Path("output/quickvc"),
+    output_dir: Path = Path("output/quickvc"),
+    output_name: Optional[str] = None,
 ) -> None:
+    # Initialize
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     logger.info("Initializing QuickVC")
     quickvc = QuickVC(
-        config_path=config,
-        checkpoint_path=checkpoint,
+        config_path=config_path,
+        checkpoint_path=checkpoint_path,
         device=device,
     )
 
-    os.makedirs(outdir, exist_ok=True)
-
+    # Run inferece
     logger.info("Synthesizing...")
-    generated_np, sr = quickvc.convert_voice(source, target)
+    with Timer() as t:
+        generated_np, sr = quickvc.convert_voice(source, target)
 
+    # Compute RTF
+    audio_duration = len(generated_np) / sr
+    rtf = t.elapsed / audio_duration
+
+    logger.info(
+        f"Elapsed time: {t.elapsed:.4f}s, Audio duration: {audio_duration:.2f}s, RTF: {rtf:.4f} iRTF: {1 / rtf:.2f}"
+    )
+
+    # Save generated audio
     logger.info("Saving generated audio...")
-    timestamp = time.strftime("%m-%d_%H-%M", time.localtime())
-    output_path = Path(outdir) / f"generated_{timestamp}.wav"
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    if output_name:
+        output_path = Path(output_dir) / output_name
+    else:
+        timestamp = time.strftime("%m-%d_%H-%M", time.localtime())
+        output_path = Path(output_dir) / f"generated_{timestamp}.wav"
+
     sf.write(output_path, generated_np, sr, format="wav")
     logger.info(f"Saved to '{output_path}'")
